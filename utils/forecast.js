f1dd3a17e29663eb0224f34c4776fe6858975a78
Rubén Acevedo <rubenfsolorzano@hotmail.com>
2022-12-03 00:43:21 -0300

    chore: add .env

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const forecast = async (coordinates) => {
-  const url = `https://api.darksky.net/forecast/8479bc15b3f20e3e03b19acc96462daf/${coordinates[0]}, ${coordinates[1]}?units=si`;
+  const url = `https://api.darksky.net/forecast/${process.env.FORECAST_TOKEN}/${coordinates[0]}, ${coordinates[1]}?units=si`;
   return await axios
     .get(url, {
       headers: {
--- utils/getLocationInfo.js
+++ utils/getLocationInfo.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const axios = require("axios");
 
 const getLocationInfo = async (address) => {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoicnViZW5hY2V2ZWRvIiwiYSI6ImNrN3dudmI3MjAzdzEzbG50dTdhbTNndzYifQ.gRwqI-b06A_dXIB9FyEYdA`;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${process.env.LOCATION_TOKEN}`;
   return await axios
     .get(url, {
       headers: {
